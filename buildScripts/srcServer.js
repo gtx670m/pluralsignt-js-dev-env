da1cf3c4065dc9289b6c936a381164d99f26df9d
Quang Le <quang.le@infonam.com>
2018-08-10 16:32:52 +0700

    module 9 work in progress

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -1,9 +1,18 @@
-var express = require('express');
-var path = require('path');
-var open = require('open');
+import express from 'express';
+import path from 'path';
+import open from 'open';
+// var open = require('open');
+import webpack from 'webpack';
+import config from '../webpack.config.dev';
+/* eslint-disable no-console */
+const port = 3000;
+const app = express();
+const complier = webpack(config);
 
-var port = 3000;
-var app = express();
+app.use(require('webpack-dev-middleware')(complier, {
+  noInfo: true,
+  publicPath: config.output.publicPath
+}));
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../src/index.html'));
---
+++ buildScripts/startMessage.js
@@ -0,0 +1,4 @@
+import chalk from 'chalk';
+
+console.log(chalk.green('Starting app in dev node...')); // eslint-disable-line no-console
+
